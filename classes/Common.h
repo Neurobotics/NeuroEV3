b35c1b66c8bf1b7d2c9739aa7a0a6f2b739c113e
Dmitry Konyshev <d.konyshev@neurobotics.ru>
2024-12-20 15:34:17 +0300

    Common: add enum getters

--- classes/Common.h
+++ classes/Common.h
@@ -4,6 +4,7 @@
 #include <QString>
 #include <QIcon>
 #include "ui/IconLabel.h"
+#include <QMetaEnum>
 
 const QString MEDITATION = "meditation";
 const QString CONCENTRATION = "concentration";
@@ -27,12 +28,35 @@ public:
 
     static QString motorString(int motorIndex);
 
+    template <typename T> static QString EnumToString(T value)
+    {
+        QMetaEnum metaEnum = QMetaEnum::fromType<T>();
+        return QString(metaEnum.valueToKey(value));
+    }
+
+    template <typename T> static QStringList EnumTextValues()
+    {
+        QMetaEnum metaEnum = QMetaEnum::fromType<T>();
+        QStringList values;
+        for (int i = 0, n = metaEnum.keyCount(); i<n; i++) {
+            values << QString(metaEnum.key(i));
+        }
+        return values;
+    }
+
+    template <typename T> static QList<int> EnumIntValues()
+    {
+        QMetaEnum metaEnum = QMetaEnum::fromType<T>();
+        QList<int> values;
+        for (int i = 0, n = metaEnum.keyCount(); i<n; i++) {
+            values << metaEnum.value(i);
+        }
+        return values;
+    }
+
 protected:
     Common();
     static Common* _instance;
 };
-
-
-
 
 #endif // COMMON_H
