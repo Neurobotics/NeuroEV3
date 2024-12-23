1932acc34940d5a3cc7e226643b2c7b91b916a62
Dmitry Konyshev <d.konyshev@neurobotics.ru>
2024-12-23 15:40:38 +0300

    ComDeviceManualControl: add slider for speed

--- com/ui/ComDeviceManualControl.cpp
+++ com/ui/ComDeviceManualControl.cpp
@@ -3,9 +3,21 @@
 #include <QGridLayout>
 #include <QCoreApplication>
 #include <QLabel>
+#include "ui/ImpovedSlider.h"
 
 ComDeviceManualControl::ComDeviceManualControl(ComDevice *com, QWidget *parent) : QWidget(parent)
 {
+    auto speedSlider = new ImprovedSlider(Qt::Horizontal);
+    speedSlider->setRange(-10, 10);
+    speedSlider->setFixedWidth(208);
+    speedSlider->setTickInterval(2);
+    speedSlider->setTickPosition(QSlider::TickPosition::TicksAbove);
+    speedSlider->setSingleStep(1);
+    speedSlider->setPageStep(1);
+    connect(speedSlider, QOverload<int>::of(&QSlider::valueChanged), [=](int value) {
+        m_com->sendSpeed(value * 10);
+    });
+
     auto func_btn = [=](QString command, QString text, QString style = "") {
         auto btn = new QPushButton(text);
         btn->setFixedSize(60, 60);
@@ -15,9 +27,10 @@ ComDeviceManualControl::ComDeviceManualControl(ComDevice *com, QWidget *parent) 
             btn->setStyleSheet(style);
         }
         connect(btn, &QPushButton::clicked, [=]() {
+            speedSlider->setValue(0);
             if (m_com) {
                 m_com->sendCommand(command);
-            }
+            }            
         });
         return btn;
     };
@@ -30,11 +43,15 @@ ComDeviceManualControl::ComDeviceManualControl(ComDevice *com, QWidget *parent) 
     grid->addWidget(func_btn(ComDevice::TURNRIGHT, "→", "QPushButton { font-size: 24px }"), 1, 2);
     grid->addWidget(func_btn(ComDevice::BACKWARDS, "↓", "QPushButton { font-size: 24px }"), 2, 1);
 
-    grid->addWidget(new QLabel(" "), 3, 0);
+    grid->addWidget(new QLabel(" "), grid->rowCount(), 0);
 
+    int r = grid->rowCount();
     for (int i = 0; i<3; i++) {
-        grid->addWidget(func_btn("Custom" + QString::number(i+1), "Custom \n" + QString::number(i+1)), 4, i);
+        grid->addWidget(func_btn("Custom" + QString::number(i+1), "Custom \n" + QString::number(i+1)), r, i);
     }
+
+    grid->addWidget(new QLabel(" "), grid->rowCount(), 0);
+    grid->addWidget(speedSlider, grid->rowCount(), 0, 1, grid->columnCount());
 
     auto lay = new QVBoxLayout(this);
     lay->addWidget(central, 0, Qt::AlignCenter);
